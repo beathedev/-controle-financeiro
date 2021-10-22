2d07032e03c4353faf177e64a9d2b752507fd0b2
beathedev <beatrizfrancisconi@souunisuam.com.br>
2021-10-22 17:35:18 -0300

    Saldo total, receitas e despesas do dummy array funcionando.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const transactionsUL = document.querySelector('#transactions');
-
+const incomeDisplay = document.querySelector('#money-plus');
+const expenseDisplay = document.querySelector('#money-minus');
+const balanceDisplay = document.querySelector('#balance');
 const dummyTransactions = [
     { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
     { id: 2, name: 'Salário', amount: 300 },
@@ -8,7 +10,6 @@ const dummyTransactions = [
 ]
 
 const addTransactionIntoDOM = transaction => {
-
     const operator = transaction.amount < 0 ? '-' : '+'
     const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
     const amountWithoutOperator = Math.abs(transaction.amount)
@@ -22,8 +23,30 @@ const addTransactionIntoDOM = transaction => {
     transactionsUL.append(li)
 }
 
+const updateBalanceValues = () => {
+    const transactionsAmount = dummyTransactions
+        .map(transaction => transaction.amount)
+    const total = transactionsAmount
+        .reduce((accumulator, transaction) => accumulator + transaction, 0)
+        .toFixed(2)
+    const income = transactionsAmount
+        .filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value)
+        .toFixed(2)
+    const expense = transactionsAmount
+        .filter(value => value < 0)
+        .reduce((accumulator, value) => accumulator + value)
+        .toFixed(2)
+
+    balanceDisplay.textContent = `R$  ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
+
+}
+
 const init = () => {
     dummyTransactions.forEach(addTransactionIntoDOM)
 }
 
+updateBalanceValues()
 init()
